eaffdfea72b8516c8fc0a7bcc6853620e6861ec3
Loopystcc <3270947368@qq.com>
2024-01-28 02:16:22 +0800

    mobile device capability
    by adding the same eventlistener but the event changed to 'touchmove'

--- script.js
+++ script.js
@@ -1,91 +1,98 @@
 const particleArray = [];
 const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext('2d');
+
 let hue = 0;
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-
-addEventListener('resize',function(){
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-})
-
-addEventListener('click',function(event){
-    mouse.x = event.x;
-    mouse.y = event.y;
-    for(let i =0;i<10;i++){
-        particleArray.push(new Particle());
-    }
-})
-
-addEventListener("mousemove",function(event){
-    mouse.x = event.x;
-    mouse.y = event.y;
-    for(let i =0;i<10;i++){
-        particleArray.push(new Particle());
-    }
-})
 
 const mouse = {
     x: undefined,
     y: undefined,
 }
 
+addEventListener('resize',function(){
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+})
+addEventListener('mousemove',function(event){
+    mouse.x = event.x;
+    mouse.y = event.y;
+    for(let i=0;i<10;i++){
+        particleArray.push(new Particle());
+    }
+})
+addEventListener('touchmove',function(event){
+    mouse.x = event.x;
+    mouse.y = event.y;
+    for(let i=0;i<10;i++){
+        particleArray.push(new Particle());
+    }
+})
+addEventListener('click',function(event){
+    mouse.x = event.x;
+    mouse.y = event.y;
+    for(let i=0;i<20;i++){
+        particleArray.push(new Particle());
+    }
+})
+
+
 class Particle{
     constructor(){
         this.x = mouse.x;
         this.y = mouse.y;
+        this.size = Math.random() * 8 + 1;
         this.speedX = Math.random() * 3 - 1.5;
         this.speedY = Math.random() * 3 - 1.5;
-        this.size = Math.random() * 15 + 1;
-        this.color = 'hsl(' + hue +',100%,50%)';
+        this.color = 'hsl(' + hue + ',100%,50%)';
     }
     update(){
         this.x += this.speedX;
         this.y += this.speedY;
-        if(this.size>0.2) this.size -= 0.1;
+        if(this.size > 0.2){
+            this.size -= 0.1;
+        }
     }
     draw(){
         ctx.fillStyle = this.color;
         ctx.beginPath();
-        ctx.arc(this.x,this.y,this.size,0,Math.PI * 2);
+        ctx.arc(this.x,this.y,this.size,0,Math.PI*2);
         ctx.fill();
     }
 }
 
-
 function handleParticles(){
-    for(let i =0;i<particleArray.length;i++){
+    for(let i=0;i<particleArray.length;i++){
         particleArray[i].update();
         particleArray[i].draw();
-        for(let j = i; j<particleArray.length; j++){
+        for(let j=i;j<particleArray.length;j++){
             const dx = particleArray[i].x - particleArray[j].x;
             const dy = particleArray[i].y - particleArray[j].y;
             const distance = Math.sqrt(dx * dx + dy * dy);
-            if(distance < 50){
+            if(distance < 100){
                 ctx.beginPath();
                 ctx.strokeStyle = particleArray[i].color;
-                ctx.lineWidth = particleArray[i].size * 0.2;
+                ctx.lineWidth = 0.2;
                 ctx.moveTo(particleArray[i].x,particleArray[i].y);
                 ctx.lineTo(particleArray[j].x,particleArray[j].y);
                 ctx.stroke();
             }
         }
-        if(particleArray[i].size <= 0.3){
-                particleArray.splice(i, 1);
-                i--;
+        if(particleArray[i].size < 0.3){
+            particleArray.splice(i,1);
+            i--;
         }
     }
 }
 
+
 function animate(){
     ctx.clearRect(0,0,canvas.width,canvas.height);
-    // ctx.fillStyle = 'rgba(0,0,0,0.02)';
-    // ctx.fillRect(0,0,canvas.width,canvas.height);
     handleParticles();
-    hue++;
     requestAnimationFrame(animate);
+    hue++;
 }
 
 animate();
